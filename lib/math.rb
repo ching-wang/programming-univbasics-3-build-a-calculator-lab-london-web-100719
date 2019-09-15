b33b65782c50edf8ce10716316a352aaf9a8b730
Qing Wang <chingwang.qw@gmail.com>
2019-09-15 18:20:25 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,26 +3,21 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  if result == num1 - num2
-  puts result
+  
 end
 
 def division(num1, num2)
-   if result == num1 / num2
-  puts result
+   
 end
 
 def multiplication(num1, num2)
-    if result == num1 * num2
-  puts result
+  
 end
 
 def modulo(num1, num2)
-    if result == num1 % num2
-  puts result
+    
 end
 
 def square_root(num)
-   result = math.sqrt(num)
-  puts result
+   
 end
