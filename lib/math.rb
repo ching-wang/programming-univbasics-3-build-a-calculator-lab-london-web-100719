9b38df0272d63eae2b9a21bf4c621fa6b3659b5e
Qing Wang <chingwang.qw@gmail.com>
2019-09-15 18:11:22 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,12 +9,12 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-   if sum == num1 - num2
+   if sum == num1 / num2
   puts sum
 end
 
 def multiplication(num1, num2)
-    if sum == num1 - num2
+    if sum == num1 * num2
   puts sum 
 end
 
